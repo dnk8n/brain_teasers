6e33d1e7828a88b30494c914873707047d3ac5dc
Dean Kayton <dean.kayton@opendna.ai>
2017-09-15 07:03:12 +0200

    simplify

--- src/puzzle_001/puzzle_solution.py
+++ src/puzzle_001/puzzle_solution.py
@@ -1,9 +1,8 @@
-unique_ints = [i for i in range(10)]
-for A in unique_ints:
-    for B in unique_ints:
-        for C in unique_ints:
-            for D in unique_ints:
-                for E in unique_ints:
+for A in range(10):
+    for B in range(10):
+        for C in range(10):
+            for D in range(10):
+                for E in range(10):
                     if ((10000 * A + 1000 * B + 100 * C + 10 * D + 1 * E) * 4 ==
                          10000 * E + 1000 * D + 100 * C + 10 * B + 1 * A
                             and len({A, B, C, D, E}) == 5):
@@ -12,4 +11,3 @@ for A in unique_ints:
                         print('C =', C)
                         print('D =', D)
                         print('E =', E)
-
