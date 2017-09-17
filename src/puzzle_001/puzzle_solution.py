b1de08ef76a59ccac60b91e29c7c8d0ec424fe6e
Dean Kayton <dean.kayton@opendna.ai>
2017-09-17 12:49:28 +0200

    More concise solution

--- src/puzzle_001/puzzle_solution.py
+++ src/puzzle_001/puzzle_solution.py
@@ -1,13 +1,8 @@
-for A in range(10):
-    for B in range(10):
-        for C in range(10):
-            for D in range(10):
-                for E in range(10):
-                    if ((10000 * A + 1000 * B + 100 * C + 10 * D + 1 * E) * 4 ==
-                         10000 * E + 1000 * D + 100 * C + 10 * B + 1 * A
-                            and len({A, B, C, D, E}) == 5):
-                        print('A =', A)
-                        print('B =', B)
-                        print('C =', C)
-                        print('D =', D)
-                        print('E =', E)
+import itertools
+for i in itertools.permutations(range(10), 5):
+    if int(''.join(map(str, i))) * 4 == int(''.join(map(str, reversed(i)))):
+        print('A =', i[0])
+        print('B =', i[1])
+        print('C =', i[2])
+        print('D =', i[3])
+        print('E =', i[4])
